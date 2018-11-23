5ea611dc4de3b0702f03f1be3b9b3d718fe4f12f
Aurimas Bachanovicius <aurimas.bachanovicius@metasite.net>
2018-11-23 04:32:57 +0200

    files

---
+++ Bash/sav3/third.sh
@@ -0,0 +1,62 @@
+close=0
+while [[ ${close} -eq 0 ]]; do
+	echo -e "Pasirinkite paveiksliuka: \n a, b, c, d, e \n arba 0 jei norite baigti programa: "
+	read task
+
+
+	case ${task} in
+	a)
+		for ((i=1; i <= 5; i++)); do
+			for ((j=1; j<=${i}; j++)); do
+				echo -n ${i}
+			done
+			echo ""
+		done
+		;;
+	b)	
+		for ((i=1; i <= 5; i++)); do
+			for ((j=1; j<=${i}; j++)); do
+				echo -n ${j}
+			done
+			echo ""
+		done
+		;;
+	c)
+		for ((i=1; i <= 5; i++)); do
+			for ((j=1; j<=${i}; j++)); do
+				echo -n "|"
+			done
+			echo -n "_"
+			echo ""
+		done
+		;;
+	d)
+		for ((i=1; i <= 5; i++)); do
+			for ((j=1; j<=${i}; j++)); do
+				echo -n "*"
+			done
+			echo ""
+		done
+		;;
+	e)
+		for ((i=1; i <= 5; i++)); do
+			for ((j=1; j<=${i}; j++)); do
+				echo -n "*"
+			done
+			echo ""
+		done
+		for ((i=5; i >= 1; i--)); do
+			for ((j=1; j<=${i}; j++)); do
+				echo -n "*"
+			done
+			echo ""
+		done
+		;;
+	0)
+		close=1
+		;;
+	*)
+		echo "pasirinktas blogas paveiksliukas"
+		;;
+	esac
+done
